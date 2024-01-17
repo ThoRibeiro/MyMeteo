214025c8111e9d9f9c477fbf88e9be9c60c7281b
tribeiro@auchan.fr <thomasribeiro123456@gmail.com>
2024-01-17 16:22:02 +0100

    Exercice 1 : Avoir la température de lille

---
+++ src/data/weather.ts
@@ -0,0 +1,22 @@
+const WEATHER_FOR_CITIES = [
+  { city: "Lille", country: "France", temperatureCelsius: -2, weatherCode: 2 },
+  { city: "Paris", country: "France", temperatureCelsius: -1, weatherCode: 45 },
+  { city: "Reims", country: "France", temperatureCelsius: -4, weatherCode: 0 },
+];
+
+export function getTemperatureForCity(city: string) {
+    const cityWeather = WEATHER_FOR_CITIES.find(
+      (weather) => weather.city.toLowerCase() === city.toLowerCase()
+    );
+  
+    if (cityWeather) {
+      console.log(`La température à ${cityWeather.city} est de ${cityWeather.temperatureCelsius}°C`);
+      return {
+        city: cityWeather.city,
+        temperature: cityWeather.temperatureCelsius,
+      };
+    } else {
+      console.log(`La température pour ${city} n'est pas disponible.`);
+      return null;
+    }
+  }
